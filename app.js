7b45af66df4b9fcffee5d89c4f6feb9cb292fd44
likeroge <likeroge@gmail.com>
2025-01-08 12:25:27 +0400

    add posts

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/posts", async (req, res) => {
-  const { data: posts } = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts",
-  );
-  res.status(200).send(posts);
+  try {
+    const { data: posts } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
+    res.status(200).send(posts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.get("/users", (req, res) => {
